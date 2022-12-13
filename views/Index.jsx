ca18ed8971c7fb72f52c72bed37cfdd07f5baa4e
charlesK <115595915+charlesknapp@users.noreply.github.com>
2022-12-12 18:45:22 -0700

    Added CSS Styling to the Index & Show Route Pages

--- views/Index.jsx
+++ views/Index.jsx
@@ -1,32 +1,46 @@
 import React, { Component } from 'react'
-  const myStyle = {
-    color: '#ffffff',
-    backgroundColor: '#ce4a00',
-    padding: '10px',
-    fontSize: '20px',
-    borderRadius: '7px',
-    fontFamily: 'monospace'
-  };
 
+// Base Styling
+  const myStyle = {
+        color: '#ffffff',
+        background: 'orange',
+        padding: '8px',
+        fontSize: '20px',
+        borderRadius: '7px',
+        fontFamily: 'monospace',
+        textAlign: 'center',
+        maxWidth: '968px',
+        margin: '0 auto',
+    };
+// List Styling
+  const listingStyle = {
+        listStyle: 'none',
+        padding: '30px',
+        borderRadius: '4px',
+        textAlign: 'center'
+    };
+// ListItem Styling
+    const listItemStyle = {
+        padding: '10px 0 10px 0',
+        textalign: 'center'
+    };
+    
 export default class Index extends Component {
   render() {
     const {pokemon} = this.props;
     console.log(pokemon)
     return (
         <div style={myStyle}>
-            <p className="title">See All The Pokemon!</p>
-            
-            {/* pokemon  list */}
-            <ul>
-            {pokemon.map((pokemon, id) => {
-            return (
-            <li>
-                <a href={`/pokemon/${id}`}> {pokemon.name}</a>
-            </li>
-            );
-            })}
-        </ul>
-
+                <p className="title"><a href="/"><img src="https://archives.bulbagarden.net/media/upload/4/4b/Pok%C3%A9dex_logo.png" alt="PokeDex Logo" /></a></p>
+                <ul style={listingStyle}>
+                    {pokemon.map((pokemon, id) => {
+                    return (
+                        <li style={listItemStyle}>
+                            <a href={`/pokemon/${id}`}> {pokemon.name}</a>
+                        </li>
+                    );
+                })}
+                </ul>
         </div>
     )
   }
--- views/Show.jsx
+++ views/Show.jsx
@@ -1,15 +1,44 @@
 import React from "react";
 import pokemon from "../models/pokemon";
 
+// CSS STYLING
+const imageStyling = {
+    padding: '8px',
+    borderRadius: '10px',
+    // backgroundColor: '#2a2a2a',
+    width: '250px',
+    height: 'auto',
+    marginTop: '-1%',
+    mixBlendMode: 'multiply'
+  };
+// CENTERED STYLING
+const centeredStyling = {
+    background: 'orange',
+    padding: '8px',
+    fontSize: '20px',
+    borderRadius: '7px',
+    fontFamily: 'monospace',
+    textAlign: 'center',
+    maxWidth: '968px',
+    margin: '0 auto',
+}
+
 class Show extends React.Component {
     render () {
         const pokemon = this.props.pokemon
         console.log(this.props.pokemon);
         return (
-            <div>
-                <h1 className="title">Show Route</h1>
-                <p>{pokemon.name} is <img src={pokemon.img} alt={pokemon.name}></img></p>
-                <a href="/" className="backToHome">Back To Homepage</a>
+            <div style={centeredStyling}>
+                <a href="/" className="homeLink"><img src="https://archives.bulbagarden.net/media/upload/4/4b/Pok%C3%A9dex_logo.png" alt="PokeDex Logo" /></a>
+                <h1 className="title">Gotta Catch 'Em All</h1>
+                <h2 className="pokeName">{pokemon.name}</h2>
+                <img style={imageStyling} src={pokemon.img} alt={pokemon.name}></img>
+                <br />
+                <br />
+                <br />
+                <div className="footer">
+                    <a href="/" className="backToHome">Back</a>
+                </div>
             </div>
         )
     }
